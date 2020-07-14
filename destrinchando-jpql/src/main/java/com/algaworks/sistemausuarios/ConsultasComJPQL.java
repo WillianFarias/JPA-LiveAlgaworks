fdf7925cfe7bc8ea1a1f1eae9bfd6b7bd3a3959e
williansousafarias@gmail.com <williansousafarias@gmail.com>
2020-07-14 14:13:14 -0300

    Consulta por id e consulta utilizando Query

--- destrinchando-jpql/src/main/java/com/algaworks/sistemausuarios/ConsultasComJPQL.java
+++ destrinchando-jpql/src/main/java/com/algaworks/sistemausuarios/ConsultasComJPQL.java
@@ -2,10 +2,7 @@ package com.algaworks.sistemausuarios;
 
 import com.algaworks.sistemausuarios.model.Usuario;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 import java.util.List;
 
 public class ConsultasComJPQL {
@@ -16,22 +13,34 @@ public class ConsultasComJPQL {
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Usuarios-PU");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
-        primeiraConsulta(entityManager);
+        primeirasConsultas(entityManager);
 
 
         entityManager.close();
         entityManagerFactory.close();
     }
 
-    public static void primeiraConsulta(EntityManager entityManager){
+    public static void primeirasConsultas(EntityManager entityManager){
 
+        //PESQUISA POR TODOS USUÁRIOS
         String jpql = "SELECT u FROM Usuario u";
         //Query tipada
         //primeiro parametro é passada uma string jpql e no segundo o que se espera receber com a consulta
         //neste caso um usuario
         TypedQuery<Usuario> typedQuery = entityManager.createQuery(jpql, Usuario.class);
         List<Usuario> lista = typedQuery.getResultList();
-
         lista.forEach(u -> System.out.println(u.getId() + ", " + u.getNome()));
+
+        //CONSUTAR POR USUÁRIO ESPECIFICO
+        String jpqlSing = "SELECT u FROM Usuario u WHERE u.id = 1";
+        TypedQuery<Usuario> typedQuerySingle = entityManager.createQuery(jpqlSing, Usuario.class);
+        Usuario usuario = typedQuerySingle.getSingleResult();
+        System.out.println(usuario.getId() + ", " + usuario.getNome());
+
+        //CONSUTAR POR USUÁRIO ESPECIFICO UTILIZANDO APENAS QUERY
+        String jpqlSingCast = "SELECT u FROM Usuario u WHERE u.id = 2";
+        Query querySingle = entityManager.createQuery(jpqlSingCast);
+        Usuario usuario2 = (Usuario) querySingle.getSingleResult();
+        System.out.println(usuario2.getId() + ", " + usuario2.getNome());
     }
 }
